149784f51ee830b0c53f2a1b9e5b88fa833173e1
zaw <123zawyenaing45@gmail.com>
2022-06-06 20:55:58 +0630

    move httpresponse codes to views.py

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,18 +1,9 @@
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import path
-
-def dashboard(request):
-    return HttpResponse("Dashboard Page")
-
-def about(request):
-    return HttpResponse("About Page")
-
-def contacts(request):
-    return HttpResponse("Contacts Page")
+from . import views
 
 urlpatterns = [
-    path('',dashboard),
-    path('about/',about),
-    path('contacts',contacts),
+    path('',views.dashboard),
+    path('about/',views.about),
+    path('contacts',views.contacts),
 ]
--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def dashboard(request):
+    return HttpResponse("Dashboard Page")
+
+def about(request):
+    return HttpResponse("About Page")
+
+def contacts(request):
+    return HttpResponse("Contacts Page")
