35f8dcf2996fa5558a02d87e6ffd99fec98f6b07
zaw <123zawyenaing45@gmail.com>
2022-06-06 20:40:39 +0630

    creating routes

--- App/urls.py
+++ App/urls.py
@@ -14,8 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+def dashboard(request):
+    return HttpResponse("Dashboard Page")
+
+def about(request):
+    return HttpResponse("About Page")
+
+def contacts(request):
+    return HttpResponse("Contacts Page")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',dashboard),
+    path('about/',about),
+    path('contacts',contacts),
 ]
